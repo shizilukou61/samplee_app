2a6fed91f4303db3a21191514052b9ff0795a0a1
shizilukou61 <brala61@gmail.com>
2013-11-05 16:59:35 +0800

    some modify about models/user.rb and user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,39 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExaMPle.Com" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save 
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_fo.org example.user@foo.
+                     foo@foo_baz.com foo@bar+baz.com foo@bax..com]
+      addresses.each do |invalid_address|
+        #走这步测试时会运行models/user.rb的代码验证email正则
+        @user.email = invalid_address
+
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org first.list@foo.jp a+b@baz.cn foo@foo.com]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
   describe "when name is too long" do 
     before { @user.name = "a" * 51 } 
     it { should_not be_valid }
